912ef8af4308adf606cb2262ad859d9fb7bff048
Ayeshmantha Wijayagunathilaka <47180051+ayesh99747@users.noreply.github.com>
2020-06-15 09:17:54 +0530

    Added Bucket Sort Implementation

---
+++ Bucket Sort/Bucket Sort.py
@@ -0,0 +1,33 @@
+def bucketSort(array):
+    bucket = []
+
+    # Create empty buckets
+    for i in range(len(array)):
+        bucket.append([])
+
+    # Insert elements into their respective buckets
+    for j in array:
+        index_b = int(10 * j)
+        bucket[index_b].append(j)
+
+    # Sort the elements of each bucket
+    for i in range(len(array)):
+        bucket[i] = sorted(bucket[i])
+
+    # Get the sorted elements
+    k = 0
+    for i in range(len(array)):
+        for j in range(len(bucket[i])):
+            array[k] = bucket[i][j]
+            k += 1
+    return array
+
+
+# Test code
+array = [8, 7, 2, 1, 0, 9, 6]
+size = len(array)
+print('Unsorted Array')
+print(array)
+bucketSort(array)
+print('Sorted Array')
+print(array)
